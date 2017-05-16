cf291aa3ad6a97744e1478fc36aaaeae35e2d282
Aleksey <pohodnyaa@gmail.com>
2017-05-16 11:26:32 +0700

    add destroy_user(user_id)

--- lib/kazoo-ruby-sdk/api_session.rb
+++ lib/kazoo-ruby-sdk/api_session.rb
@@ -82,5 +82,12 @@ module KazooRubySdk
         request.headers['X-Auth-Token'] = auth_token
       end.body
     end
+
+    def destroy_user(user_id)
+      pipe.delete do |request|
+        request.url "accounts/#{account_id}/users/#{user_id}"
+        request.headers['X-Auth-Token'] = auth_token
+      end.body
+    end
   end
 end
