56dcf18773b64e5950b68023fe5881978701372b
Aleksey <pohodnyaa@gmail.com>
2017-05-17 11:15:00 +0700

    Fix session define

--- lib/kazoo-ruby-sdk.rb
+++ lib/kazoo-ruby-sdk.rb
@@ -4,8 +4,8 @@ require 'faraday_middleware'
 require 'digest/md5'
 
 require 'kazoo-ruby-sdk/configuration'
-require 'kazoo-ruby-sdk/base'
 require 'kazoo-ruby-sdk/session'
+require 'kazoo-ruby-sdk/base'
 require 'kazoo-ruby-sdk/devices'
 require 'kazoo-ruby-sdk/users'
 
