c840dd796d3565cfb9a673bb044fd4c6f4376125
Aleksey <pohodnyaa@gmail.com>
2017-05-17 12:50:53 +0700

    init base

--- lib/kazoo-ruby-sdk/base.rb
+++ lib/kazoo-ruby-sdk/base.rb
@@ -1,10 +1,12 @@
 module KazooRubySdk
   class Base
-    @auth_token = KazooRubySdk::Session.auth_token
-    @api_url = KazooRubySdk::Session.api_url
-    @realm = KazooRubySdk::Session.realm
-    @account_id = KazooRubySdk::Session.account_id
-    @owner_id = KazooRubySdk::Session.owner_id
-    @pipe = KazooRubySdk::Session.api_url
+    def initialize
+      @auth_token = KazooRubySdk::Session.auth_token
+      @api_url = KazooRubySdk::Session.api_url
+      @realm = KazooRubySdk::Session.realm
+      @account_id = KazooRubySdk::Session.account_id
+      @owner_id = KazooRubySdk::Session.owner_id
+      @pipe = KazooRubySdk::Session.api_url
+    end
   end
 end
