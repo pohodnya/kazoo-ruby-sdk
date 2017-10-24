0400ff2aad6e3e0be529e4002edce772c7b55b7c
Max <maxim21214@gmail.com>
2017-10-24 18:00:16 +0700

    Update kazoo-ruby-sdk.rb

--- lib/kazoo-ruby-sdk.rb
+++ lib/kazoo-ruby-sdk.rb
@@ -1,15 +1,16 @@
-require 'kazoo-ruby-sdk/version'
+require 'digest/md5'
 require 'faraday'
 require 'faraday_middleware'
-require 'digest/md5'
+require 'kazoo-ruby-sdk/version'
 
-require 'kazoo-ruby-sdk/configuration'
-require 'kazoo-ruby-sdk/session'
 require 'kazoo-ruby-sdk/base'
-require 'kazoo-ruby-sdk/devices'
-require 'kazoo-ruby-sdk/users'
 require 'kazoo-ruby-sdk/callflows'
 require 'kazoo-ruby-sdk/cdrs'
+require 'kazoo-ruby-sdk/configuration'
+require 'kazoo-ruby-sdk/devices'
+require 'kazoo-ruby-sdk/phone_numbers'
+require 'kazoo-ruby-sdk/session'
+require 'kazoo-ruby-sdk/users'
 
 module KazooRubySdk
   class << self
